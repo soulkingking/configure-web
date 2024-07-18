6bb4a974b3f6b5394bfa7f71b4e4b0870750fb70
Soul <1210156434@qq.com>
2024-07-18 21:54:47 +0800

    修改插件说明

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -3,6 +3,8 @@ import 'tdesign-vue-next/es/style/index.css';
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css';
 import 'virtual:uno.css';
 
-const install = (app) => {};
+const install = (app) => {
+  // 这里可以添加 Vue 插件的安装逻辑，但当前没有使用任何参数
+};
 
 export default install;

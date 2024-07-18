d66d18e5d2198650489857157e16aa041fa9ac08
Soul <caobin@jajachina.com>
2024-07-18 21:30:18 +0800

    新增插件

---
+++ src/plugin/index.js
@@ -0,0 +1,8 @@
+// 引入组件库的少量全局样式变量
+import 'tdesign-vue-next/es/style/index.css';
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css';
+import 'virtual:uno.css';
+
+const install = (app) => {};
+
+export default install;
